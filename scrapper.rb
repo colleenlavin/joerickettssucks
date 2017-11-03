ad704ef67fd449065db8170096e351db780b009b
Christopher Glynn Pelech <christopherglynnpelech@gmail.com>
2017-11-02 19:51:23 -0500

    get all url for a given page result

--- scrapper.rb
+++ scrapper.rb
@@ -1,9 +1,22 @@
 # TwitterTrends1.rb
 require 'rubygems'
 require 'rest_client'
+require "uri"
+
+def getPage(page)
+  offset = (50 * page).to_s
+  url = 'http://archive.is/offset=' + offset + '/http://laist.com/*'
+  RestClient.get(url)
+end
+
  
-url = 'http://archive.is/http://laist.com/%2A'
- 
-response = RestClient.get(url)
- 
-puts response.body
+page = getPage(0)
+
+
+for url in URI.extract(page)
+  if url.include? 'laist'
+    puts url
+  end
+end
+# => ["http://foo.example.org/bla", "mailto:test@example.com"]
+
