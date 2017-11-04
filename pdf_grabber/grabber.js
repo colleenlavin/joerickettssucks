f746c25c583c2754e5bd3246b6cfab23c01a6aea
Parker Higgins <parker@parkerhiggins.net>
2017-11-04 13:42:11 -0400

    some tweaks to the grabber, fixing filenames and separating out dna styles

--- pdf_grabber/grabber.js
+++ pdf_grabber/grabber.js
@@ -3,16 +3,21 @@ const slugify = require('slugify');
 const argv = require('minimist')(process.argv.slice(2));
 
 const url = argv.url;
+const outdir = argv.outdir || '.';
 let offset = url.indexOf('.com');
-const filename = slugify(url.substring(offset + 5));
+
+const filename = slugify(url.substring(offset + 5).replace(/\//g,'-'));
 
 (async () => {
-	const browser = await puppeteer.launch();
-	const page = await browser.newPage();
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
     await page.setExtraHTTPHeaders({'Accept-Charset': 'utf-8'});
-	await page.goto(url, {timeout: 0});
-	await page.emulateMedia('screen');
-	await page.pdf({displayHeaderFooter: true, margin: {top: '.5in', bottom: '.5in', left: '.5in', right: '.5in'}, printBackground: true, path: filename+ '.pdf'});
+    await page.goto(url, {timeout: 0});
+    if (url.includes('dnainfo.com')) {
+        await page.addStyleTag({path: 'dnainfotweaks.css'})
+    }
+    await page.emulateMedia('screen');
+    await page.pdf({displayHeaderFooter: true, margin: {top: '.5in', bottom: '.5in', left: '.5in', right: '.5in'}, printBackground: true, path: outdir + '/' + filename+ '.pdf'});
 
-	await browser.close();
+    await browser.close();
 })();
